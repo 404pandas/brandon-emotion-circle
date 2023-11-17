8b83cedc8ab41446847a9fb9b74f93204f758407
404pandas <113853550+404pandas@users.noreply.github.com>
2023-11-17 09:03:34 -0500

    Example animation

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.scrollspy');
+    var instances = M.ScrollSpy.init(elems, options);
+    var options = 
+  });
